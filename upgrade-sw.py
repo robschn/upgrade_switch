1b17ee99940713c9c120f09a29488ac2cc4307db
Robert Schneider <robmschn@gmail.com>
2019-12-19 10:55:16 -0500

    reload is not working right. Removing for now

--- upgrade-sw.py
+++ upgrade-sw.py
@@ -32,40 +32,22 @@ swi_numbers = []
 for x in parse_swi:
     swi_numbers.append(x['switch'])
 
-print(f'Found found {swi_numbers[-1]} switch(es) in the stack!')
+print(f'Found {swi_numbers[-1]} switch(es) in the stack!')
 # ftp the file to flash1:
+print(f'Copying {ios_name} to flash:/')
 net_connect.send_command(f'copy ftp://user:user@{ftp_server}/{ios_name} flash:/', expect_string=']?')
 net_connect.send_command('\n', expect_string='#')
 
 # copy from flash1: to flash#:
 for swi_num in swi_numbers:
-    print(f'Copying {ios_name} to flash{swi_num}. Please wait...')
-    if swi_num != 1
+    if swi_num != 1:
+        print(f'Copying {ios_name} to flash{swi_num}. Please wait...')
         net_connect.send_command(f'copy flash:/{ios_name} flash{swi_num}:/', expect_string=']?')
         net_connect.send_command('\n', expect_string='#')
 
 print('\nSetting boot path and writing memory.')
 net_connect.send_config_set(f'boot system switch all flash:/{ios_name}')
 net_connect.send_command('write mem')
-
-print('Here is the current switch priority\n')
-sh_swi = net_connect.send_command('show switch')
-print(sh_swi)
-
-reload_prompt = input('Would you like to proceed with a reload? [y/n]: ')
-if reload_prompt == 'y':
-    reload_time = input('Enter reload time HH:MM. If now, press "enter": ')
-    if reload_time == '':
-        print('Reloading now!')
-        net_connect.send_command('reload', expect_string='[confirm]')
-        net_connect.send_command('\n')
-        
-    else:
-        print(f'Reload set for {reload_time}')
-        net_connect.send_command(f'reload at {reload_time}')
-        net_connect.send_command('reload', expect_string='[confirm]')
-        net_connect.send_command('\n')
-        
 
 print('Update finished! Exiting now..')
 exit()
